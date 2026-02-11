7c8abcea21d631779a774fb5a136bffe5b7d8135
KaueCMP2 <kaue.s.campos@aluno.senai.br>
2026-02-10 20:53:07 -0500

    :heavy_plus_sign: create and add database script of LoginDb

---
+++ tela Login/DATABASE/script/LoginDbScript.sql
@@ -0,0 +1,55 @@
+CREATE DATABASE LoginOAuth2
+GO
+
+Use LoginOAuth2
+GO
+
+CREATE TABLE UserTb
+(
+Id INT IDENTITY PRIMARY KEY,
+Nome VARCHAR (60) not null,
+Age DATETIME not null,
+UserPassword VARBINARY (32) not null,
+Picture VARBINARY (MAX) null,
+DateSign DATETIME DEFAULT GETDATE()
+)
+GO
+
+CREATE TABLE UserDataLog
+(
+UserDataLogId INT IDENTITY PRIMARY KEY,
+UserId INT not null,
+DescriptionLog NVARCHAR (30),
+LastPassword VARBINARY (32) null,
+LastPicture VARBINARY (MAX) null,
+DateOcurrendLog DATETIME DEFAULT GETDATE()
+)
+GO
+
+CREATE TABLE UserSystemLog
+(
+UserSystemLog INT IDENTITY PRIMARY KEY,
+UserId INT not null,
+DescriptionLog NVARCHAR (30),
+DateOcurrendLog DATETIME DEFAULT GETDATE()
+)
+GO
+
+CREATE TABLE AutentificationMethod
+(
+Id INT IDENTITY PRIMARY KEY,
+MethodName VARCHAR (10),
+MethodDescription NVARCHAR (50)
+)
+GO
+
+CREATE TABLE UserAutentificationMethod
+(
+UserId INT,
+MethodId INT,
+CONSTRAINT PK_UserId_MethodId PRIMARY KEY (UserId, MethodId),
+CONSTRAINT FK_UserAutentificationMethod_UserTb_UserId FOREIGN KEY (UserId) REFERENCES UserTb (Id),
+CONSTRAINT FK_UserAutentificationMethod_AutentificationMethod_MethodId FOREIGN KEY (MethodId) REFERENCES AutentificationMethod (Id)
+
+)
+GO
